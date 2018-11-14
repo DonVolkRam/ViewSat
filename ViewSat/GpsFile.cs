c792fe060d968de250dec628fea5c5a3c3fc87ca
DonVolkRam <success.vka@gmail.com>
2018-11-14 14:12:41 +0300

    Работает чтение пакетов Начало обработки исключительных ситуаций

--- ViewSat/GpsFile.cs
+++ ViewSat/GpsFile.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
+
 
 namespace ViewSat
 {
@@ -20,16 +22,20 @@ namespace ViewSat
         public byte BaseTime { get; set; }
         public byte Chksum { get; set; }
 
+
+
     };
 
     struct PG01E            //Geodesic Position
-    {        
+    {
         public double Fi { get; set; }
         public double Lam { get; set; }
         public double H { get; set; }
         public float Ps { get; set; } //Position SEP
         public byte Type { get; set; }
         public byte Chksum { get; set; }
+
+
     };
 
     class GpsFile
@@ -48,10 +54,12 @@ namespace ViewSat
                 Console.WriteLine("Успешное открытие файла" + filename);
                 Log.Write("Успешное открытие файла" + filename);
 
-                FileName = filename;                
+                FileName = filename;
 
                 Read(/*ref gpsReader*/);
                 GpsStream.Close();
+                Console.WriteLine("Чтение завершено");
+                Log.Write("Чтение завершено");
             }
             catch (ArgumentException e)
             {
@@ -63,7 +71,7 @@ namespace ViewSat
                 Console.WriteLine("Ошибка: " + e.Message);
                 Log.Write("Ошибка: " + e.Message);
             }
-           
+
             #endregion
         }
         public void Read(/*ref FileStream gpsStream*/)
@@ -73,15 +81,71 @@ namespace ViewSat
             Log.Write("Открытие файла gps");
             //            char[] buf = new char[5];
             byte[] buf = new byte[5];
-            List<PG01E> PG = new List<PG01E>();
-            int i;
-            while ((i = GpsStream.ReadByte()) != -1)
+            List<string> Packages = new List<string>
             {
-                GpsStream.Position--;
-                //memcpy(com_temp, com_temp + 1, 5);
-                GpsStream.Read(buf, 0, 5);
-                GpsStream.Position--
-                //gpsStream.ReadBlock(buf, 0, 5);
+                "~~0",
+                "RD0",
+                "PG0",
+                "VG0",
+                "SG0",
+                "DP0",
+                "PS0",
+                "TO0",
+                "SI0",
+                "SS0",
+                "EL0",
+                "AZ0",
+                "EC0",
+                "FC0",
+                "||0"
+            };
+
+            List<PG01E> PG = new List<PG01E>();
+            DateTime Start = new DateTime(); // Время запуска
+            Start = DateTime.Now;
+            DateTime Stoped = new DateTime(); //Время окончания
+            TimeSpan Elapsed = new TimeSpan();
+            string CurBuf;
+            string xPackSize;
+            int PackSize=0;
+            string LatPackage = "";
+            while ((GpsStream.Read(buf, 0, 5)) != -1)
+            {
+
+                Stoped = DateTime.Now; // Стоп (Записываем время)
+                Elapsed = Stoped.Subtract(Start);
+                Console.Clear();
+                Console.WriteLine(Elapsed.Ticks.ToString());
+                Console.WriteLine($"{GpsStream.Position} байт из {GpsStream.Length}");
+
+                CurBuf = Encoding.Default.GetString(buf);
+                xPackSize = Encoding.Default.GetString(buf, 3, 2);
+                try
+                {
+                    PackSize = Convert.ToInt32(xPackSize, 16) + 1;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine($"Проблема чтения {LatPackage} пакета");
+                    
+                    break;
+                }
+                //GpsStream.Position -= 4;
+
+                for (int i = 0; i < Packages.Count; i++)
+                {
+                    if (CurBuf == Packages[i] + xPackSize)
+                    {
+                        GpsStream.Position += PackSize;
+                        LatPackage = Packages[i];
+                        continue;
+                    }
+
+                }
+
+                Start = DateTime.Now; // Старт (Записываем время)
+
             }
         }
     }
--- ViewSat/Program.cs
+++ ViewSat/Program.cs
@@ -14,8 +14,8 @@ namespace ViewSat
         {
             LogFile Log = new LogFile("LogFile.txt");
             Console.WriteLine("Введите имя файла");
-            string filename = Console.ReadLine();
-            filename = "..\\..\\10_1_1_C1.gps";
+//            string filename = Console.ReadLine();
+            string filename = "..\\..\\10_1_1_C1.gps";
             GpsFile Stream = new GpsFile(filename);            
             Console.Read();
         }
